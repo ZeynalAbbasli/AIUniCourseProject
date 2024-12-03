3ea925c2de6ad764db62516d484db9470233954b
ZeynalAbbasli <abbaslizeynal772@gmail.com>
2024-12-03 21:24:38 +0400

    Updated the dataset.py

--- dataset.py
+++ dataset.py
@@ -10,12 +10,11 @@ import random
 from pathlib import Path
 
 class custom_dataset(Dataset):
-    def __init__(self, mode = "train", root = './dataset/splitted/', transformsBasic = None, transformsAugment = None):
+    def __init__(self, mode = "train", root = './dataset/splitted/', transformsBasic = None):
         super().__init__()
         self.mode = mode
         self.root = root
         self.transformsBasic = transformsBasic
-        self.transformsAugment = transformsAugment
         
         #select split
         self.folder = os.path.join(self.root, self.mode)
@@ -33,24 +32,26 @@ class custom_dataset(Dataset):
             llst = []
             for image in os.listdir(os.path.join(self.folder, self.class_list[class_id])):
                 count += 1
-                self.image_list.append((os.path.join(self.folder, self.class_list[class_id], image), 'none'))
-                llst.append((os.path.join(self.folder, self.class_list[class_id], image), 'none'))
+                self.image_list.append(os.path.join(self.folder, self.class_list[class_id], image))
+                llst.append(os.path.join(self.folder, self.class_list[class_id], image))
+                # self.image_list.append((os.path.join(self.folder, self.class_list[class_id], image), 'none'))
+                # llst.append((os.path.join(self.folder, self.class_list[class_id], image), 'none'))
                 label = np.zeros(len(self.class_list))
                 label[class_id] = 1.0
                 self.label_list.append(label)
-            if(count < 100 and self.mode == 'train'):
-                numrand = random.randint(100, 120)
-                extrasmpl = numrand - count
-                if(extrasmpl <= count):
-                    for i in range(extrasmpl):
-                        self.image_list.append((llst[i][0],'augm'))
-                        self.label_list.append(label)
-                else:
-                    k = math.ceil(extrasmpl/count)
-                    for j in range(len(llst)):
-                        for _ in range(k):
-                            self.image_list.append((llst[j][0],'augm'))
-                            self.label_list.append(label)
+            # if(count < 100 and self.mode == 'train'):
+            #     numrand = random.randint(100, 120)
+            #     extrasmpl = numrand - count
+            #     if(extrasmpl <= count):
+            #         for i in range(extrasmpl):
+            #             self.image_list.append((llst[i][0],'augm'))
+            #             self.label_list.append(label)
+            #     else:
+            #         k = math.ceil(extrasmpl/count)
+            #         for j in range(len(llst)):
+            #             for _ in range(k):
+            #                 self.image_list.append((llst[j][0],'augm'))
+            #                 self.label_list.append(label)
 
                 
         
@@ -62,8 +63,6 @@ class custom_dataset(Dataset):
         image = Image.open(image_name)
         if(self.transformsBasic):
             image = self.transformsBasic(image)
-        if(self.mode == 'train' and self.transformsAugment and self.image_list[index][1] == 'augm'):
-            image = self.transformsAugment(image)
         label = torch.tensor(label)
         
         return image, label
