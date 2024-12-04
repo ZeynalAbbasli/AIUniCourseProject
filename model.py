14dbf0785b137d845420c589c48dbf09e60000c0
ZeynalAbbasli <abbaslizeynal772@gmail.com>
2024-12-04 22:30:52 +0400

    modified model.py

--- model.py
+++ model.py
@@ -8,16 +8,14 @@ class ExModel(nn.Module):
     def __init__(self):
         super().__init__()
                 
-        self.resnet18 = model.resnet18(pretrained=False)
-        self.resnet18 = torch.nn.Sequential(*(list(self.resnet18.children())[:-1]))    
-        
-        self.classifier = torch.nn.Linear(512, 8)
+        self.resnet18 = model.resnet18()
+        num_ftrs = self.resnet18.fc.in_features
+        self.resnet18.fc = nn.Linear(num_ftrs, 8)
 
 
     def forward(self, image):
         # Get predictions from ResNet18
-        resnet_pred = self.resnet18(image).squeeze()
-        out = self.classifier(resnet_pred)
+        out = self.resnet18(image)
         
         return out
     
